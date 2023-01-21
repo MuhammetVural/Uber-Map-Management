b6355f3c86b1c24fc8a8c8469bcd9188c1c67b36
Muhammet Vural <muhammetvural94@gmail.com>
2023-01-21 11:33:11 +0300

    widgetlar getirildi authentication işlemleri başlatıldı

---
+++ lib/widgets/loading_dialog.dart
@@ -0,0 +1,30 @@
+
+
+
+
+import 'package:delivery_by_bike_riders/widgets/progress_bar.dart';
+import 'package:flutter/material.dart';
+
+class LoadingDialog extends StatelessWidget {
+
+  final String? message;
+  const LoadingDialog({super.key, this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      key: key,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          circularProgress(),
+          SizedBox(height: 10,),
+          Text('${message!},  Please wait...'),
+
+        ],
+      ),
+
+
+    );
+  }
+}
