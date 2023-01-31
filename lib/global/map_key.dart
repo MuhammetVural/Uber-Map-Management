b0459e7a8bcb80e0e4646ab9326a9fbffc8d340f
Muhammet <muhammetvural@anadolu.edu.tr>
2023-01-31 11:27:03 +0300

    map key gitignore landı

--- lib/global/map_key.dart
+++
@@ -1,3 +0,0 @@
-String mapApiKey = "AIzaSyCalOpqAMn-ubjg_EfYbywge3paFfuHdgc";
-String androidMapKey = "AIzaSyBKaScDpiZ613f1FuLRIdeZMVRl34ZwyHI";
-String iosMapKey = "AIzaSyDHND1GTn2X8eqaJLG5qnf9IhyD0B7klPY";
